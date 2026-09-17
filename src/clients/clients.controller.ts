c12de90ad67c685c128e6d07de9ac96afb3c2218
ivanpinheiro23 <ivan.pinheiro@cognizant.com>
2026-09-17 10:46:04 -0300

    Update get

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -31,7 +31,7 @@ export class ClientsController {
     return this.clientsService.findOne(id);
   }
 
-  @Patch(':id')
+  @Patch(':id-test')
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateClientDto: UpdateClientDto,
