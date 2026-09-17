71f9ee8c044a61bc4e67a81462b24357f6ff7e79
ivanpinheiro23 <ivan.pinheiro@cognizant.com>
2026-09-17 11:03:40 -0300

    Update

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -27,7 +27,7 @@ export class ClientsController {
     return this.clientsService.findAll();
   }
 
-  @Get('search')
+  @Get('search-test')
     search(@Query('name') name: string) {
     return this.clientsService.search(name);
   }
