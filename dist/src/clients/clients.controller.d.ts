076de8f85ff77e01dc5afeff93cc217279abe57c
Rodrigo Lopes <rodrigo.lopes2@cognizant.com>
2026-09-17 11:57:53 -0300

    action i.a rules

---
+++ dist/src/clients/clients.controller.d.ts
@@ -0,0 +1,55 @@
+import { ClientsService } from './clients.service';
+import { CreateClientDto } from './dto/create-client.dto';
+import { UpdateClientDto } from './dto/update-client.dto';
+export declare class ClientsController {
+    private readonly clientsService;
+    constructor(clientsService: ClientsService);
+    create(createClientDto: CreateClientDto): Promise<{
+        name: string;
+        taxId: string;
+        email: string | null;
+        phone: string | null;
+        createdAt: Date;
+        updatedAt: Date;
+        id: number;
+    }>;
+    findAll(): import(".prisma/client").Prisma.PrismaPromise<{
+        name: string;
+        taxId: string;
+        email: string | null;
+        phone: string | null;
+        createdAt: Date;
+        updatedAt: Date;
+        id: number;
+    }[]>;
+    search(name: string): Promise<{
+        name: string;
+        taxId: string;
+        email: string | null;
+        phone: string | null;
+        createdAt: Date;
+        updatedAt: Date;
+        id: number;
+    }[]>;
+    findOne(id: number): Promise<{
+        name: string;
+        taxId: string;
+        email: string | null;
+        phone: string | null;
+        createdAt: Date;
+        updatedAt: Date;
+        id: number;
+    }>;
+    update(id: number, updateClientDto: UpdateClientDto): Promise<{
+        name: string;
+        taxId: string;
+        email: string | null;
+        phone: string | null;
+        createdAt: Date;
+        updatedAt: Date;
+        id: number;
+    }>;
+    remove(id: number): Promise<{
+        message: string;
+    }>;
+}
